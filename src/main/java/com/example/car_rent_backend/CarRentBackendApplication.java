880cf3f76d3a163497cd364009a06698222b8f1b
Tharaka Jayasinghe <124444782+tharaka-jayasinghe-7@users.noreply.github.com>
2024-12-05 16:13:06 +0530

    init

---
+++ src/main/java/com/example/car_rent_backend/CarRentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.car_rent_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/car_rent_backend/CarRentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.car_rent_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
