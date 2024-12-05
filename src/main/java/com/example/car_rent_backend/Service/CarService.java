317e4296dceeb10bb549b340b7bb40cc6c5c013c
Tharaka Jayasinghe <124444782+tharaka-jayasinghe-7@users.noreply.github.com>
2024-12-05 18:43:36 +0530

    get all cars sorted

--- src/main/java/com/example/car_rent_backend/Service/CarService.java
+++ src/main/java/com/example/car_rent_backend/Service/CarService.java
@@ -3,6 +3,7 @@ package com.example.car_rent_backend.Service;
 import com.example.car_rent_backend.Data.Car;
 import com.example.car_rent_backend.Data.CarRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +14,6 @@ public class CarService {
     private CarRepo carRepo;
 
     public List<Car> getAllCars() {
-        return carRepo.findAll();
+        return carRepo.findAll(Sort.by(Sort.Order.asc("price")));
     }
 }
